1cecc917e2b3ebd6320a7fdd151631dcd8c06a98
piaand <pia.m.andersin@gmail.com>
2021-01-22 16:07:49 +0200

    added logging to project

--- src/main/java/com/piaand/co2emissions/Co2EmissionsApplication.java
+++ src/main/java/com/piaand/co2emissions/Co2EmissionsApplication.java
@@ -1,13 +1,29 @@
 package com.piaand.co2emissions;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Co2EmissionsApplication {
 
+	private final LogConfigurations configurations;
+
+	public Co2EmissionsApplication(LogConfigurations configurations) {
+		this.configurations = configurations;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Co2EmissionsApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			configurations.setConfig();
+			System.out.println("Hello World!");
+		};
 	}
 
 }
