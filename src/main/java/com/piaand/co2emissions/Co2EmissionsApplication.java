fc291a0a7aa0131e2cf71f5111873d862b9ba302
piaand <pia.m.andersin@gmail.com>
2021-01-22 15:01:59 +0200

    first commit

---
+++ src/main/java/com/piaand/co2emissions/Co2EmissionsApplication.java
@@ -0,0 +1,13 @@
+package com.piaand.co2emissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2EmissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2EmissionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piaand/co2emissions/Co2EmissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piaand.co2emissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2EmissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
